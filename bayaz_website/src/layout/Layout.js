6381201748ffab96645c83d165517e186295ca4c
Vera <danny.vera@accenture.com>
2022-08-04 14:21:10 -0400

    header

--- bayaz_website/src/layout/Layout.js
+++ bayaz_website/src/layout/Layout.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
+import Header from "./Header";
+
 
 function Layout(props) {
   return (
     <div className="layout">
       <Header></Header>
       <main>{props.children}</main>
-      <Footer></Footer>
+      
     </div>
   );
 }
