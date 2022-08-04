d2e79c32b16a31c10394570559fb16e332e7a15c
Vera <danny.vera@accenture.com>
2022-08-04 13:56:43 -0400

    layout

--- bayaz_website/src/App.js
+++ bayaz_website/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
+import React from "react";
+import Layout from "./layout/Layout";
+import Shows from "./components/Shows";
+
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Layout>
+        <Shows />
+      </Layout>
     </div>
   );
 }
