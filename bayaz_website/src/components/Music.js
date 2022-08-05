a9bf359f01fb6df758d207946fbadb8f09d4a2ab
Dudolevitch <mike.dudolevitch@accenture.com>
2022-08-05 16:47:03 -0400

    music widgets embedded

--- bayaz_website/src/components/Music.js
+++ bayaz_website/src/components/Music.js
@@ -5,7 +5,8 @@ import './Music.css'
 
 const Music = () => {
   return (
-    <div className="music paddingTop borderBox">Music</div>
+    <div className="music paddingTop borderBox"><iframe title="seasides" style={{margin: "1vw", border: 0, width: "30vw", height: 470}} src="https://bandcamp.com/EmbeddedPlayer/album=1203236600/size=large/bgcol=ffffff/linkcol=e99708/tracklist=false/transparent=true/" seamless><a href="https://thenuclears.bandcamp.com/album/seasides">Seasides by The Nuclears</a></iframe><iframe title="nothin to do" style={{margin: "1vw" ,border: 0, width: "30vw", height: 470}} src="https://bandcamp.com/EmbeddedPlayer/track=3156155333/size=large/bgcol=ffffff/linkcol=e99708/tracklist=false/transparent=true/" seamless><a href="https://thenuclears.bandcamp.com/track/i-just-wanna-have-nothin-to-do">I Just Wanna Have Nothin&#39; To Do by The Nuclears</a></iframe>
+    <iframe title="record high" style={{margin: "1vw", border: 0, width: "30vw", height: 470}} src="https://bandcamp.com/EmbeddedPlayer/album=1609386363/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://thenuclears.bandcamp.com/album/record-high">Record High by The Nuclears</a></iframe></div>
   )
 }
 
