3fc2494ba0f5fae7f56191d47811937305d5fa10
Rabin <max.rabin@accenture.com>
2022-08-08 11:30:35 -0400

    install react-bootstrap

--- bayaz_website/src/components/Footer.js
+++ bayaz_website/src/components/Footer.js
@@ -7,6 +7,9 @@ const Footer = () => {
       <div class="container text-center">
         <p id="footer-text">© 2022 Bayaz. Site created by Danny Vera, Max Rabin, Hathor Hamilton, and Mike Dudolevitch</p>
       </div>
+      <div class="container text-center" style={{color: 'red'}}>
+        HELLOOOO WORLDDD
+      </div>
     </div>
 
   )
