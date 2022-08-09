6fe97e94566bbec459b866e1ef635237169497c0
Vera <danny.vera@accenture.com>
2022-08-09 10:38:53 -0400

    SongKick integration in Shows/tourdates

--- bayaz_website/src/components/Shows.js
+++ bayaz_website/src/components/Shows.js
@@ -1,28 +1,24 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './Component.css'
 import './Shows.css'
 
-function Shows() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "//widget.songkick.com/10225887/widget.js";
-    script.async = true;
-    document.body.appendChild(script);
-      return () => {
-          document.body.removeChild(script);
-      }
-  }, []);
+export function AddLibrary(urlOfTheLibrary) {
+  const script = document.createElement('script');
+  script.src = urlOfTheLibrary;
+  script.async = true;
+  document.body.appendChild(script);
+}
 
+function Shows() {
   return (
     <div id="shows" className='page-wrapper component'>
         <div className="tourDates">
             <h1 className="section-title">Shows</h1>
             <a href="https://www.songkick.com/artists/10225887" className="songkick-widget" data-theme="dark" data-track-button="on" data-detect-style="true" data-font-color="#FCF3CB" data-background-color="transparent" data-locale="en">Bayaz (US) tour dates</a>
+            {AddLibrary("//widget.songkick.com/10225887/widget.js")}
         </div>
     </div>
   )
 }
-
-
 
 export default Shows
