1c45070f68bbd3cb247dc6c1ff036e1a7be9c9cf
Vera <danny.vera@accenture.com>
2022-08-09 10:19:46 -0400

    SongKick integration in Shows/tourdates

--- bayaz_website/src/components/Shows.js
+++ bayaz_website/src/components/Shows.js
@@ -1,16 +1,28 @@
-//a Formatted <ul> that I can update - can also pull from Songkick /Bandsintown
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Component.css'
 import './Shows.css'
 
-const Shows = () => {
+function Shows() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "//widget.songkick.com/10225887/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+      return () => {
+          document.body.removeChild(script);
+      }
+  }, []);
+  
   return (
     <div id="shows" className='page-wrapper component'>
-      <div className="">
-        <h1 className="section-title">Shows</h1>
-      </div>
+        <div className="tourDates">
+            <h1 className="section-title">Shows</h1>
+            <a href="https://www.songkick.com/artists/10225887" className="songkick-widget" data-theme="dark" data-track-button="on" data-detect-style="true" data-font-color="#FCF3CB" data-background-color="transparent" data-locale="en">Bayaz (US) tour dates</a>
+        </div>
     </div>
   )
 }
+
+
 
 export default Shows
