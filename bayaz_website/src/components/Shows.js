0a26fcfef90dea2ab1c30512bd8acd68964c32e9
Dudolevitch <mike.dudolevitch@accenture.com>
2022-08-09 11:25:07 -0400

    continue formatting shows widget

--- bayaz_website/src/components/Shows.js
+++ bayaz_website/src/components/Shows.js
@@ -14,7 +14,7 @@ function Shows() {
     <div id="shows" className='page-wrapper component'>
         <div className="tourDates">
             <h1 className="section-title">Shows</h1>
-            <a href="https://www.songkick.com/artists/10225887" className="songkick-widget" data-theme="dark" data-track-button="on" data-detect-style="true" data-font-color="#FCF3CB" data-background-color="transparent" data-locale="en">Bayaz (US) tour dates</a>
+            <a href="https://www.songkick.com/artists/10225887" style={{marginLeft: "20vw"}} className="songkick-widget" data-theme="dark" data-track-button="on" data-detect-style="true" data-font-color="#FCF3CB" data-text-shadow="2px 2px 2px #C96D03" data-font-size="1.1em" data-background-color="transparent" data-locale="en">Here's a list of our upcoming shows- grab some tickets and we'll see ya there!</a>
             {AddLibrary("//widget.songkick.com/10225887/widget.js")}
         </div>
     </div>
