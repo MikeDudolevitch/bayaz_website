cc61ab25c5bf6b2da87092186d4462afecdd6748
Dudolevitch <mike.dudolevitch@accenture.com>
2022-08-09 09:33:46 -0400

    add tourbox integration to shows

--- bayaz_website/src/components/Shows.js
+++ bayaz_website/src/components/Shows.js
@@ -8,6 +8,10 @@ const Shows = () => {
     <div id="shows" className='page-wrapper component'>
       <div className="">
         <h1 className="section-title">Shows</h1>
+        <br></br>
+        <a href="https://www.songkick.com/artists/10225887" class="songkick-widget" data-theme="light" data-track-button="on" data-detect-style="true" data-background-color="transparent" data-locale="en">Bayaz (US) tour dates</a>
+<script src="//widget.songkick.com/10225887/widget.js"></script>
+
       </div>
     </div>
   )
