61ba0766fdb0f50c978c1fbbf599ba65a6aa1ba0
Dudolevitch <mike.dudolevitch@accenture.com>
2022-08-10 15:30:26 -0400

    correct form fields in contact form

--- bayaz_website/src/components/Contact.js
+++ bayaz_website/src/components/Contact.js
@@ -4,30 +4,31 @@
 import React from 'react'
 import './Component.css'
 import './Contact.css'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { Button, Form } from 'react-bootstrap';
 
 const Contact = () => {
   return (
     <div id="contact" className='page-wrapper component'>
       <div className="">
         <h1 className="section-title">Contact</h1>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
+        <Form id='contact-form'>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label className='form-label'>First Name</Form.Label>
+            <Form.Control className='form-control' type="text" placeholder="First Name" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Label className='form-label'>Last Name</Form.Label>
+            <Form.Control className='form-control' type="text" placeholder="Last Name" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className='form-label'>Email address</Form.Label>
+              <Form.Control className='form-control' type="email" placeholder="Enter email" />
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Form.Group className="mb-3">
+            <Form.Label className='form-label'>Message</Form.Label>
+            <Form.Control as="textarea" rows={3}  className='form-control' type="textarea" placeholder="Write Your Message" />
+          </Form.Group>
+          <Button style={{marginRight: 'auto', marginLeft: 'auto', display: 'block', backgroundColor: '#E0514C', color: '#FCF3CB', border: 'thin solid red'}} variant="primary" type="submit">
             Submit
           </Button>
     </Form>
