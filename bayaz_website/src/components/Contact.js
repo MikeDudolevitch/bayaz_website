22bd7d1954c3fd3f77013830afd575de1e142117
Dudolevitch <mike.dudolevitch@accenture.com>
2022-08-10 11:38:26 -0400

    create form for contact page

--- bayaz_website/src/components/Contact.js
+++ bayaz_website/src/components/Contact.js
@@ -4,12 +4,37 @@
 import React from 'react'
 import './Component.css'
 import './Contact.css'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Contact = () => {
   return (
     <div id="contact" className='page-wrapper component'>
       <div className="">
         <h1 className="section-title">Contact</h1>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    <br>
+    </br>
+    {/* Put Social Icons here */}
       </div>
     </div>
     
