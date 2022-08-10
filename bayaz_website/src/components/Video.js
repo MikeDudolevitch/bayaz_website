6469b6080c0f965f8a18ad409f93a3367672e037
Vera <danny.vera@accenture.com>
2022-08-10 12:16:02 -0400

    videos with youtube and centering them

--- bayaz_website/src/components/Video.js
+++ bayaz_website/src/components/Video.js
@@ -2,14 +2,52 @@
 import React from 'react'
 import './Component.css'
 import './Video.css'
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 const Video = () => {
   return (
     <div id='video' className='page-wrapper component'>
       <h1 className="section-title">Video</h1>
-      <div className="">
-        
-      </div>
+      <Container fluid="md">
+        <Row className="">
+          <Col  className="videoContainer">      
+            <iframe  className="youtube" src='https://www.youtube.com/embed/BId86m-TCj4'
+              frameborder='0'
+              allow='autoplay; encrypted-media'
+              allowfullscreen
+              title='video'
+              />
+            </Col>
+            <Col className="videoContainer">      
+            <iframe  className="youtube" src='https://www.youtube.com/embed/BId86m-TCj4'
+              frameborder='0'
+              allow='autoplay; encrypted-media'
+              allowfullscreen
+              title='video'
+            />
+          </Col>
+        </Row>
+        <Row className="">
+          <Col className="videoContainer">      
+            <iframe  className="youtube" src='https://www.youtube.com/embed/BId86m-TCj4'
+              frameborder='0'
+              allow='autoplay; encrypted-media'
+              allowfullscreen
+              title='video'
+              />
+            </Col>
+            <Col className="videoContainer">      
+              <iframe  className="youtube"
+                src='https://www.youtube.com/embed/BId86m-TCj4'
+                frameborder='0'
+                allow='autoplay; encrypted-media'
+                allowfullscreen
+                title='video'
+              />
+          </Col>
+        </Row>
+      </Container>      
     </div>
   )
 }
