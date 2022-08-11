931893469894a4310abd9ea52d42750f0610ef1e
Vera <danny.vera@accenture.com>
2022-08-11 08:54:35 -0400

    video modal, still need to work on dimensions

---
+++ bayaz_website/src/components/Modal.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+// import { useRef } from "react";
+import "./Modal.css";
+
+function Modal(props) {
+  const {
+    modalVis,
+    setModalVis,
+    title,
+    URL,
+    // text
+  } = props;
+
+//   const [urlSource, setUrlSource] = useState(URL);
+  const [urlSource, setUrlSource] = useState(URL);
+  useEffect(() => {
+    const body = document.body;
+    if (modalVis === "visible") {
+      body.style.overflowY = "hidden";
+      setUrlSource(URL);
+
+    } else {
+      body.style.overflowY = "auto";
+      setUrlSource("");
+
+    }
+  }, [modalVis, URL]);
+
+  return (
+    <div>
+      <div className={`modalParent ${modalVis}`}>
+        <div className={`modalCustom ${modalVis}`}>
+          <button
+            className="modalClose"
+            aria-label="Close Project Modal Box"
+            onClick={() => {
+              setModalVis("");
+              setUrlSource("");
+            }}
+          >
+            &times;
+          </button>
+          <h2>{title}</h2>
+          <iframe 
+              src={urlSource} 
+              alt={title}               
+              frameBorder='0'
+              allow='autoplay; encrypted-media'
+              allowFullScreen
+              title={title} 
+              ></iframe>
+          <div className="modalButtons">
+            <button
+              className="modalButton"
+              onClick={() => {
+                setModalVis("");
+                setUrlSource("");
+              }}
+            >
+              Close Window
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- bayaz_website/src/components/Video.js
+++ bayaz_website/src/components/Video.js
@@ -1,53 +1,87 @@
 // Youtube Tiles, opens Modal like Danny's portfolio project
-import React from 'react'
+import React, { useState } from 'react'
 import './Component.css'
 import './Video.css'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Modal from "./Modal";
+import {Container, Row, Col} from 'react-bootstrap';
+
 const Video = () => {
+    // set each modal to "visible" if they are to open
+    const [modalVis1, setModalVis1] = useState("");
+    const [modalVis2, setModalVis2] = useState("");
+    const [modalVis3, setModalVis3] = useState("");
+    const [modalVis4, setModalVis4] = useState("");
+
   return (
+    
     <div id='video' className='page-wrapper component'>
       <h1 className="section-title">Video</h1>
-      <Container fluid="md">
-        <Row className="">
-          <Col  className="videoContainer">      
-            <iframe  className="youtube" src='https://www.youtube.com/embed/BId86m-TCj4'
-              frameborder='0'
-              allow='autoplay; encrypted-media'
-              allowfullscreen
-              title='video'
-              />
-            </Col>
-            <Col className="videoContainer">      
-            <iframe  className="youtube" src='https://www.youtube.com/embed/BId86m-TCj4'
-              frameborder='0'
-              allow='autoplay; encrypted-media'
-              allowfullscreen
-              title='video'
+      <Container flow="md">
+        <Row className="d-flex flex-row">
+          <Col  className="videoContainer">    
+            <img
+              className="youtube"
+              src="./pics/Video1.png"
+              alt="Zegema Beach"
+              onClick={() => {setModalVis1("visible")}}
+            />
+          </Col>
+          <Col className="videoContainer">     
+            <img
+              className="youtube"
+              src="./pics/Video2.png"
+              alt="Lightspeed Getaway"
+              onClick={() => {setModalVis2("visible")}}
             />
           </Col>
         </Row>
-        <Row className="">
+        <Row className="d-flex flex-row">
           <Col className="videoContainer">      
-            <iframe  className="youtube" src='https://www.youtube.com/embed/BId86m-TCj4'
-              frameborder='0'
-              allow='autoplay; encrypted-media'
-              allowfullscreen
-              title='video'
-              />
+            <img
+              className="youtube"
+              src="./pics/Video3.png"
+              alt="Lightspeed Getaway"
+              onClick={() => {setModalVis3("visible")}}
+            />
             </Col>
-            <Col className="videoContainer">      
-              <iframe  className="youtube"
-                src='https://www.youtube.com/embed/BId86m-TCj4'
-                frameborder='0'
-                allow='autoplay; encrypted-media'
-                allowfullscreen
-                title='video'
-              />
+            <Col className="videoContainer">    
+            <img
+              className="youtube"
+              src="./pics/Video4.png"
+              alt="Lightspeed Getaway"
+              onClick={() => {setModalVis4("visible")}}
+            />
           </Col>
         </Row>
       </Container>      
+      <Modal
+        modalVis={modalVis1}
+        setModalVis={setModalVis1}
+        URL='https://www.youtube.com/embed/BId86m-TCj4?enablejsapi=1&version=3&playerapiid=ytplayer'
+        title="Zegema Beach"
+        text=""
+      />
+      <Modal
+        modalVis={modalVis2}
+        setModalVis={setModalVis2}
+        URL='https://www.youtube.com/embed/WZcGftHr5Xg?enablejsapi=1&version=3&playerapiid=ytplayer'
+        title="Lightspeed Getaway"
+        text=""
+      />
+      <Modal
+        modalVis={modalVis3}
+        setModalVis={setModalVis3}
+        URL='https://www.youtube.com/embed/5mhtyW6gxMY?enablejsapi=1&version=3&playerapiid=ytplayer'
+        title="I Just Wanna Have Nothin to Do"
+        text=""
+      />
+      <Modal
+        modalVis={modalVis4}
+        setModalVis={setModalVis4}
+        URL='https://www.youtube.com/embed/mOoTwRQzB80?enablejsapi=1&version=3&playerapiid=ytplayer'
+        title="Back to my Girl"
+        text=""
+      />
     </div>
   )
 }
