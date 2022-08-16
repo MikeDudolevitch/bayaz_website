272c88070ebe138d7727e16087c9a73e9fc03299
Vera <danny.vera@accenture.com>
2022-08-16 10:12:45 -0400

    music removed inline styling

--- bayaz_website/src/components/Music.js
+++ bayaz_website/src/components/Music.js
@@ -9,11 +9,11 @@ const Music = () => {
       <h1 className="section-title">Music</h1>
       <div className="iframeContainer">
             {/* Music widgets */}
-            <iframe title="seasides" style={{margin: "1vw", border: 0, width: "30vw", height: "50vw"}} src="https://bandcamp.com/EmbeddedPlayer/album=1203236600/size=large/bgcol=ffffff/linkcol=e99708/tracklist=false/transparent=true/" seamless><a href="https://thenuclears.bandcamp.com/album/seasides">Seasides by The Nuclears</a></iframe>
+            <iframe title="seasides" className="iframeSong"  src="https://bandcamp.com/EmbeddedPlayer/album=1203236600/size=large/bgcol=ffffff/linkcol=e99708/tracklist=false/transparent=true/" seamless><a href="https://thenuclears.bandcamp.com/album/seasides">Seasides by The Nuclears</a></iframe>
             {/* Music widgets */}
-            <iframe title="nothin to do" style={{margin: "1vw" ,border: 0, width: "30vw", height: "50vw"}} src="https://bandcamp.com/EmbeddedPlayer/track=3156155333/size=large/bgcol=ffffff/linkcol=e99708/tracklist=false/transparent=true/" seamless><a href="https://thenuclears.bandcamp.com/track/i-just-wanna-have-nothin-to-do">I Just Wanna Have Nothin&#39; To Do by The Nuclears</a></iframe>
+            <iframe title="nothin to do" className="iframeSong" src="https://bandcamp.com/EmbeddedPlayer/track=3156155333/size=large/bgcol=ffffff/linkcol=e99708/tracklist=false/transparent=true/" seamless><a href="https://thenuclears.bandcamp.com/track/i-just-wanna-have-nothin-to-do">I Just Wanna Have Nothin&#39; To Do by The Nuclears</a></iframe>
             {/* Music widgets */}
-            <iframe title="record high" style={{margin: "1vw", border: 0, width: "30vw", height: "50vw"}} src="https://bandcamp.com/EmbeddedPlayer/album=1609386363/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://thenuclears.bandcamp.com/album/record-high">Record High by The Nuclears</a></iframe>
+            <iframe title="record high" className="iframeSong" src="https://bandcamp.com/EmbeddedPlayer/album=1609386363/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://thenuclears.bandcamp.com/album/record-high">Record High by The Nuclears</a></iframe>
       </div>
     </div>
   )
