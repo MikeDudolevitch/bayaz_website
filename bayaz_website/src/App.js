468b2dedb55df8f989ed75fb6b3e3d44de9cf44a
Vera <danny.vera@accenture.com>
2022-08-16 12:07:57 -0400

    make Music first default section

--- bayaz_website/src/App.js
+++ bayaz_website/src/App.js
@@ -14,8 +14,8 @@ function App() {
   return (
     <div className="App">
       <Layout>
-          <Gallery />
           <Music />
+          <Gallery />
           <Video />
           <Shows />
           <About />
