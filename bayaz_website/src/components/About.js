76c11dc0be2460865b39bdd099765eb86ce495e3
Dudolevitch <mike.dudolevitch@accenture.com>
2022-08-17 12:08:06 -0400

    metadata for sharing

--- bayaz_website/src/components/About.js
+++ bayaz_website/src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <h1 className="section-title">About</h1>
           <div id='about-paragraph'>
             <h2 className='page-content content-header'>Bayaz: The Story of Bayaz</h2>
-            <p className='page-content'>Lorem ipsum <img id='about-img' alt='placeholder pic' src='../../public/pics/BAYAZRED.jpg'></img></p>
+            <p className='page-content'>Lorem ipsum <div class='img-responsive'><img id='about-img' alt='placeholder pic' src='./pics/bayazpictveye.jpg' style={{float: 'right'}}></img></div></p>
           </div>
       </div>
     </div>
